953cc7088da468f92a9eb924e18f71aec7cad729
*SKarakayaa* <*sefakarakayaa@gmail.com*>
2019-11-10 12:15:33 +0300

    API - SWAGGER Added

--- CooksProjectCore.WebAPI/Startup.cs
+++ CooksProjectCore.WebAPI/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CooksProjectCore.WebAPI
 {
@@ -32,6 +33,11 @@ namespace CooksProjectCore.WebAPI
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Cooks API", Version = "V1" });
+            });
 
             services.AddCors(options =>
             {
@@ -71,6 +77,13 @@ namespace CooksProjectCore.WebAPI
             app.UseCors(builder => builder.WithOrigins("https://localhost:3000").AllowAnyOrigin());
             app.UseAuthentication();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cooks API V1");
+                c.RoutePrefix=string.Empty;
+            });
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
@@ -79,7 +92,7 @@ namespace CooksProjectCore.WebAPI
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "home.html");
+                    template: "cooks-api/swagger");
             });
         }
     }
