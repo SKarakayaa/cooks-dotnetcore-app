0532531de6b58694bb8776719b582bfd65020f16
*SKarakayaa* <*sefakarakayaa@gmail.com*>
2019-12-02 21:49:10 +0300

    Transaction Infrastructure is added

---
+++ CooksProjectCore.Core/Aspects/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using CooksProjectCore.Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace CooksProjectCore.Core.Aspects.Transaction
+{
+    public class TransactionAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transaction = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transaction.Complete();
+                }
+                catch (Exception)
+                {
+                    transaction.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
